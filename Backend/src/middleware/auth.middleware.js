f7b9d1b05416e176f6da1801ffa036628d113627
6hovansh <bhovanshsahu@gmail.com>
2026-05-08 17:19:38 +0530

    Move backend files into Backend folder

--- Backend/src/middleware/auth.middleware.js
+++ Backend/src/middleware/auth.middleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken")
+const blacklistTokenModel = require("../models/blacklist.model")
 
 // middleware to protect routes, check if token is valid and add user details to req.user
-function authUser(req, res, next) {
+async function authUser(req, res, next) {
     const token = req.cookies.token
 
     if (!token) {
@@ -10,9 +11,17 @@ function authUser(req, res, next) {
         })
     }
 
+    const isBlacklistedToken = await blacklistTokenModel.findOne({ token })
+
+    if (isBlacklistedToken) {
+        return res.status(401).json({
+            message: "Token is invalid, authorization denied"
+        })
+    }
+
     try {
         const decoded = jwt.verify(token, process.env.jwtSecret)
-        req.user = decoded   //here the user is new proprty/variable created here and doesnt exist before.
+        req.user = decoded   //here the user is new property/variable created here and doesnt exist before.
         next()
     } catch (err) {
         return res.status(401).json({
@@ -21,4 +30,4 @@ function authUser(req, res, next) {
     }
 }
 
-module.exports = authUser;
+module.exports = {authUser};
